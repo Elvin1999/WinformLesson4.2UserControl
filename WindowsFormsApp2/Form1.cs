e3a4b3ef2ef17d4d8af1d714a093104af24fbc1d
Elvin <camalzade_elvin@mail.ru>
2021-07-27 13:51:31 +0400

    Add project files.

---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        UserControl1 userControl1 = new UserControl1();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userControl1.Location = new Point(200, 0);
+            this.Controls.Add(userControl1);
+            this.Controls.Remove(userControl2);
+        }
+
+        UserControl2 userControl2 = new UserControl2();
+        private void button2_Click(object sender, EventArgs e)
+        {
+            userControl2.Location = new Point(200, 0);
+            this.Controls.Remove(userControl1);
+            this.Controls.Add(userControl2);
+        }
+    }
+}
---
+++ WindowsFormsApp2/UserControl2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class UserControl2 : UserControl
+    {
+        public UserControl2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.BackColor = Color.Red;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            this.BackColor = Color.SpringGreen;
+        }
+    }
+}
